2503c151fe08b36e33a168201a9d9f553aea6703
Devon Poston <dev.poston@gmail.com>
2021-03-09 20:48:39 -0800

    Working Complete Createsession Middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,22 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  //no cookies => generate session
+    //a generated session should create/append a hash to the res
+  //cookie => check for validity
+      //validity
+        //check session table for req.cookies.shortlyid
+      //creating an object with the username and userId from session table corresponding to that has
+      //append object to res
+    //invalid => delete cookie and session???
+  let hash = req.cookies.shortlyid;
+  if ( hash ) {
+    models.Sessions.get({hash: hash})
+    .then((sessionHash) => {
+
+    })
+  }
+
 };
 
 /************************************************************/
