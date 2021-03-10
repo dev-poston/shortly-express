45beb5fd80494364ffff79f2a3c0cf3782a645a2
Devon Poston <dev.poston@gmail.com>
2021-03-10 12:21:20 -0800

    Complete Createsession Middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,19 +3,44 @@ const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
   //no cookies => generate session
-    //a generated session should create/append a hash to the res
+  //a generated session should create/append a hash to the res
   //cookie => check for validity
-      //validity
-        //check session table for req.cookies.shortlyid
-      //creating an object with the username and userId from session table corresponding to that has
-      //append object to res
-    //invalid => delete cookie and session???
+  //validity
+  //check session table for req.cookies.shortlyid
+  //creating an object with the username and userId from session table corresponding to that has
+  //append object to res
+  //invalid => delete cookie and session???
   let hash = req.cookies.shortlyid;
-  if ( hash ) {
-    models.Sessions.get({hash: hash})
-    .then((sessionHash) => {
-
-    })
+  if (hash) {
+    models.Sessions.get({ hash: hash })
+      .then((session) => {
+        if (session) {
+          session['hash'] = hash;
+          req.session = session;
+          res.cookie('shortlyid', hash);
+          next();
+        } else {
+          models.Sessions.create()
+            .then((data) => {
+              models.Sessions.get({ id: data.insertId })
+                .then((hashVal) => {
+                  req.session = hashVal;
+                  res.cookie('shortlyid', req.session.hash);
+                  next();
+                });
+            });
+        }
+      });
+  } else {
+    models.Sessions.create()
+      .then((data) => {
+        models.Sessions.get({id: data.insertId})
+          .then((hashVal) => {
+            req.session = hashVal;
+            res.cookie('shortlyid', req.session.hash);
+            next();
+          });
+      });
   }
 
 };
